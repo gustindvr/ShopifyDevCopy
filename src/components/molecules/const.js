860349f80c4771267a9996d0ef0ca5885e48cab5
gustindvr <gustindvr@gmail.com>
2022-05-31 11:10:25 -0300

    feat: toolsCards segment

---
+++ src/components/molecules/const.js
@@ -0,0 +1,64 @@
+export const cardsContainer = [
+  {
+    image:
+      'https://cdn.shopify.com/shopifycloud/shopify_dev/assets/icons/48/apps-dark-47602a8ae2a6c391c946a81183b79c138aaee44275b8e815727dd1c8bee58c34.png',
+    title: 'Apps',
+  },
+  {
+    image:
+      'https://cdn.shopify.com/shopifycloud/shopify_dev/assets/icons/48/themes-dark-e19d2d10ddc0cbe4975577dcc8534d958fb57140e3b3452b98dca93e618c925e.png',
+    title: 'Themes',
+  },
+  {
+    image:
+      'https://cdn.shopify.com/shopifycloud/shopify_dev/assets/icons/48/custom-storefronts-dark-e3e3864628f324696c05ee13198a43e92ab2f5ba4f8ead9f388c37605507a7a4.png',
+    title: 'Storefronts',
+  },
+  {
+    image:
+      'https://cdn.shopify.com/shopifycloud/shopify_dev/assets/icons/48/marketplaces-dark-045cfd7473dbbcbbf650ee4ae70fad32357c0224d8ba5208e0b91f5c9e4c5960.png',
+    title: 'Marketplaces',
+  },
+];
+
+export const toolsInfo = [
+  {
+    url: 'https://cdn.shopify.com/shopifycloud/shopify_dev/assets/home/icons/icon-community-small-02dccda350b673dbd5d87a40cdd900de45ddef6f69b8dd9498c4ca9ab652cc0b.png',
+    title: 'Join a community of builders',
+  },
+  {
+    url: 'https://cdn.shopify.com/shopifycloud/shopify_dev/assets/icons/48/forums-dark-7e3726f5c27f7b8e93f54f39b8d94b355170490157d8cb17cfa1f5950fac527f.png',
+    title: 'Developer forums',
+    text: 'Q&A and advice from Shopify community experts',
+  },
+  {
+    url: 'https://cdn.shopify.com/shopifycloud/shopify_dev/assets/icons/48/slack-dark-3966a9900077357eff47003b7d1b5cff4cc900168a298d80adcecd4592d3c827.png',
+    title: '#development on Slack',
+    text: 'Collaboration and connection with Shopify employees',
+  },
+  {
+    url: 'https://cdn.shopify.com/shopifycloud/shopify_dev/assets/icons/48/twitter-dark-ba8525deb962659ea0bb824fcb5ea33ffa6ccde9ab9e5f34329e8b40a2df2194.png',
+    title: '@ShopifyDevs on Twitter',
+    text: 'Product announcements, API updates, tips and more',
+  },
+  {
+    url: 'https://cdn.shopify.com/shopifycloud/shopify_dev/assets/icons/48/changelog-dark-efd364d2cc997bafab4ed234f986456dfdc0595e61d425a5f578cccdc143cf29.png',
+    title: 'Developer Changelog',
+    text: 'The latest Shopify ecosystem updates',
+  },
+  {
+    url: 'https://cdn.shopify.com/shopifycloud/shopify_dev/assets/icons/48/youtube-dark-8f581f4c27b70bc4144dd6122f5e21665280c8d42a581af9315b68c0fe336319.png',
+    title: 'ShopifyDevs YouTube channel',
+    text: 'Tips and insights in dev and design',
+  },
+  {
+    url: 'https://cdn.shopify.com/shopifycloud/shopify_dev/assets/icons/48/discord-dark-bd4f9ca894a89382dc24e7530d50fa52f7b94364609d5954f07480a404b4971c.png',
+    title: 'ShopifyDevs Discord server',
+    text: 'Chat with other Shopify developers',
+  },
+  {
+    url: 'https://cdn.shopify.com/shopifycloud/shopify_dev/assets/icons/48/shopify-dark-7095d5908f0ad051527fb42e6e3ce43a920507e61e98b46a0b46d2884964d8cf.png',
+    title: 'Shopify Partners blog',
+    text: 'Educational resources and product previews',
+  },
+];
